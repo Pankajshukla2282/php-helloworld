6562564ce352c47a89a5f0919def3541f5a29f41
Pankaj Shukla <pankajshukla2282@gmail.com>
2020-06-19 18:35:18 +0530

    created hello branch

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 echo "This is first PHP app deployed by CI/CD";
 echo "<br/>";
 echo "Lets explore OpenShift together!";
+echo "<br/>";
 echo "Hello Branch";
+echo "<br/>";
+echo "New change";
+
 ?>
